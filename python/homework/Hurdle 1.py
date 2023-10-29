77b48f3ccea48f1475a3572bccdd53fbf26877b6
avi-friedman <avishmen@gmail.com>
2023-10-29 10:51:35 +0200

    add file

---
+++ python/homework/Hurdle 1.py
@@ -0,0 +1,26 @@
+def fanction_r():
+    for i in range(2):
+        move()
+        turn_left()
+        turn_left()
+        turn_left()
+        
+def fanction_l():
+    move()
+    turn_left()
+    
+def fanction_game():
+    for i in range (6):
+        fanction_l()
+        fanction_r()
+        fanction_l()
+        
+fanction_game()
+    
+
+    
+
+################################################################
+# WARNING: Do not change this comment.
+# Library Code is below.
+################################################################
